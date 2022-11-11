7ca551f6fe0792668dc6800cc48ace495094ddce
Edmilton Amazonas <93544259+EdmiltonSobrinho@users.noreply.github.com>
2022-11-11 16:49:24 -0300

    Solução ruim

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,24 +1,21 @@
 package model.entities;
-// solução 1 - muito ruim
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeUnit;;
 
 public class Reservation {
-	private static Integer roomNumber;
+
+	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
 	
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
-	
-	public Reservation(){
-		
-	}
-
-	
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
-	//	super();
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -36,27 +33,23 @@ public class Reservation {
 		return checkIn;
 	}
 
-	//public void setCheckIn(Date checkIn) {   // pra não deixar que as datas seja modificadas aqui
-	//	this.checkIn = checkIn;
-	//}
-
 	public Date getCheckOut() {
 		return checkOut;
-		
 	}
 
-	//public void setCheckOut(Date checkOut) {  // idem setCheckIn porquê vai ter um 
-	//	this.checkOut = checkOut;               // update - assim desde o começo do curso
-	//}                                         // um metodo separado para atualização
-	
-	// cálculo em milisegundos , normal da plaaforma java
-	// devolve a diferença de datas 
 	public long duration() {
-		long dif = checkOut.getTime() - checkIn.getTime();
-		return TimeUnit.DAYS.convert(dif, TimeUnit.MILLISECONDS); // converte dif para dias
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-
+	
 	public void updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			throw new DomainException("Reservation dates for update must be future dates");
+		}
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 	}
@@ -74,4 +67,3 @@ public class Reservation {
 			+ " nights";
 	}
 }
-
